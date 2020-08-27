04d1390323ceca4479f35d10996b678f80cac6bc
guadi1994 <guadi1994@gmail.com>
2020-08-27 15:08:44 +0200

    count ellipsis type totals

--- dataset/build_dataset.py
+++ dataset/build_dataset.py
@@ -3,6 +3,59 @@ import typer
 import xml.etree.ElementTree as ET
 import json
 from tag_maps import TAGS_SFG as tag_map
+from collections import defaultdict
+
+
+def get_function_label(node):
+    """
+    Get function label of a node.
+    """
+
+    features = [feat.get("value") for feat in node[1]]
+
+    if "functionLabels.SBJ" in features:
+        function_label = "subject"
+    elif not set(["label.VB", "label.VBD", "label.VBG", "label.VBN", "label.VBP", "label.VBZ"]).isdisjoint(set(features)):
+        function_label = "auxiliary"
+    elif "label.ADVP" in features:
+        function_label = "adjunct"
+    elif "label.NP" in features and "functionLabels.SBJ" not in features:
+        function_label = "object"
+    elif "label.VP" in features:
+        function_label = "process"
+    else:
+        function_label = "other"
+
+    return function_label
+
+
+def count_ellipsis_types(graph):
+    """
+    Count how many ellipsis of each type occur in a graph.
+    """
+
+    ellipsis_types = defaultdict(int)
+
+    for node in graph:
+        if node["tag"] == "CL":
+            auxiliary = node["ellipsis_types"]["auxiliary"]
+            adjunct = node["ellipsis_types"]["adjunct"]
+            # count adjunct + (auxiliary) + (subject) as 1 ellipsis
+            while adjunct > 0:
+                adjunct -= 1
+                if node["ellipsis_types"]["subject"] > 0:
+                    node["ellipsis_types"]["subject"] -= 1
+                if node["ellipsis_types"]["auxiliary"] > 0:
+                    node["ellipsis_types"]["auxiliary"] -= 1
+            # count auxiliary + (subject) as 1 ellipsis
+            while auxiliary > 0:
+                auxiliary -= 1
+                if node["ellipsis_types"]["subject"] > 0:
+                    node["ellipsis_types"]["subject"] -= 1
+            for type_ in node["ellipsis_types"]:
+                ellipsis_types[type_] += node["ellipsis_types"][type_]
+
+    return ellipsis_types
 
 
 def xml2graph(node, text, tag_map):
@@ -20,6 +73,7 @@ def xml2graph(node, text, tag_map):
     graph = [] # list of dictionaries representing nodes
     tracking_list = [] # list of terminal spans
     terminals = [] # list of the sentence tokens
+    parent_clause = None # keep track of the parent clause
 
     # push the root of the tree to the stack
     stack.append((node, 0))
@@ -31,7 +85,8 @@ def xml2graph(node, text, tag_map):
         parent = last[1]
         if node.tag == "Constituent":
             # get basic information from the node
-            node_type = node.get('type')                
+            node_type = node.get("type")     
+            function_label = get_function_label(node)      
             if node_type in ["Word", "Punctuation"]:
                 terminal = "yes"
                 tag = node[1][0].get('value')[6:] # pos
@@ -52,9 +107,15 @@ def xml2graph(node, text, tag_map):
             if ellipsed_node == False:
                 # create a graph node
                 graph_id = len(tracking_list)
-                graph_node = {"id": graph_id, "children": [], "parent": parent, "ellipsed_parents": [], "terminal": terminal, "tag": tag, "text": node_text}
+                # keep track of the parent clause
+                if node.get("type") == "Clause":
+                    parent_clause = graph_id
+                    ellipsis_types = {"subject": 0, "auxiliary": 0, "adjunct": 0, "object": 0, "process": 0, "other": 0}
+                else: 
+                    ellipsis_types = None
+                graph_node = {"id": graph_id, "children": [], "parent": parent, "ellipsed_parents": [], "terminal": terminal, "tag": tag, "text": node_text, "function_label": function_label, "parent_clause": parent_clause, "ellipsis_types": ellipsis_types}
                 # add node span to the tracking list
-                span = [subnode.get("id") for subnode in node.iter() if subnode.get("type") == "Word"]
+                span = [subnode.get("id") for subnode in node.iter() if subnode.get("type") in ["Word", "Punctuation"]]
                 tracking_list.append(span)
                 # add node to the graph
                 graph.append(graph_node)
@@ -79,13 +140,22 @@ def xml2graph(node, text, tag_map):
                         node["children"][child_id] = span_id  
                         # add parent to the node's ellipsed parents list
                         graph[span_id]["ellipsed_parents"].append(node["id"]) 
+                        break
         # if terminal, add node's text to the list of terminals
         if node["terminal"] == "yes":
             terminals.append(node["text"])
         # count tokens for a BERT-based system
             bert_len += 4 # left bracket + right bracket + pos + text
         elif node["terminal"] == "no":
-            bert_len += 3 # left bracket + right bracket + tag            
+            bert_len += 3 # left bracket + right bracket + tag
+
+    # count ellipsis types per clause
+    for node in graph:
+        if len(node["ellipsed_parents"]) > 0:
+            ellipsis_type = node["function_label"]
+            for parent in node["ellipsed_parents"]:
+                parent_clause = graph[parent]["parent_clause"]
+                graph[parent_clause]["ellipsis_types"][ellipsis_type] += 1
 
     # build sentence string (without duplication of ellipsis as in the original SFGbank)
     string = " ".join(terminals)
@@ -98,7 +168,7 @@ def gen_json(files, ellipsis_only, max_len):
     Generate a dictionary containing sentence strings, graphs, and whether they contain ellipsis or not.
     """
 
-    data = {"docs": []}
+    data = {"docs": [], "total_sents": 0, "ellipsis_types": defaultdict(int)}
 
     for f in files:
         doc = {"doc": f.name, "sents": []}
@@ -117,7 +187,11 @@ def gen_json(files, ellipsis_only, max_len):
             if max_len == True and bert_len > 512: # maximum length = 512
                 continue
             else:
-                doc["sents"].append({"string": string, "graph": graph, "ellipsis": ellipsis, "bert_length": bert_len})
+                doc["sents"].append({"string": string, "graph": graph, "ellipsis": ellipsis, "bert_len": bert_len})
+                data["total_sents"] += 1
+                ellipsis_types = count_ellipsis_types(graph)
+                for type_ in ellipsis_types:
+                    data["ellipsis_types"][type_] += ellipsis_types[type_]
         data["docs"].append(doc)
 
     return data
@@ -138,6 +212,11 @@ def gen_splits(input_dir, output_dir, ellipsis_only, max_len):
     train_graphs = gen_json(train_files, ellipsis_only, max_len)
     dev_graphs = gen_json(dev_files, ellipsis_only, max_len)
     test_graphs = gen_json(test_files, ellipsis_only, max_len)
+
+    # count total for each type of ellipsis
+    print("Total for each type of ellipsis:\n")
+    for type_ in train_graphs["ellipsis_types"]:
+        print(type_ + ": " + str(train_graphs["ellipsis_types"][type_] + dev_graphs["ellipsis_types"][type_] + test_graphs["ellipsis_types"][type_]))
 
     with open(output_dir.joinpath("train.json"), "w+") as train_json:
         json.dump(train_graphs, train_json)
