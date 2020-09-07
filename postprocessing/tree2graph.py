d3ad9c15e27128faa1ed3f7d4e3c6a9ef81448f2
guadi1994 <guadi1994@gmail.com>
2020-09-07 13:26:23 +0200

    postprocessing tree2graph ready

--- postprocessing/tree2graph.py
+++ postprocessing/tree2graph.py
@@ -5,77 +5,84 @@ import re
 import json
 
 
-def convert(input_file, ellipsis_method):
+def convert(input_file, right_label):
     """
     :param input_file: file containing the phrase-structure trees 
-    :param ellipsis_method: str, indicating the method for encoding ellipsis, can either be right_label or left_label
+    :param right_label: bool, whether the ellipsis has been encoded with the right label strategy or not
     :return graph: embedded dict, containing the parsed data as a graph
     """
 
     with open(input_file, "r", encoding="utf-8") as f:
         trees = f.readlines()
 
-    graph = {"sentences": []}
+    sents = {"sents": []}
 
     for tree in trees:
 
-        tokens = {}
+        graph = []
         tree_positions = {}
 
+        # build graph
         t = ParentedTree.fromstring(tree)
         for index, st in enumerate(t.subtrees()):
             tree_positions[st.treeposition()] = index # keep track of indexes & tree positions
-            token = {}
-            token["id"] = index
-            token["children"] = []
-            token["parent"] = 0
-            token["parent_ellipsed"] = []
-            token["tag"] = st.label().split("ellipsis")[0]
-            ellipsis_tags = st.label().split("ellipsis")[1:]
-            token["ellipsis_tag"] = ellipsis_tags
+            node = {}
+            node["id"] = index
+            node["children"] = []
+            node["parent"] = tree_positions[st.parent().treeposition()] if st.parent() != None else 0
+            node["parent_ellipsed"] = []
+            node["tag"] = st.label().split("-ellipsis")[0]
+            ellipsis_tags = st.label().split("-ellipsis")[1:]
+            node["ellipsis_tags"] = ellipsis_tags
+            if st.height() == 2:
+                node["terminal"] = "yes"
+                node["text"] = st.leaves()[0]
+            else:
+                node["terminal"] = "no"
+                node["text"] = ""
+            graph.append(node)  
+
+        # recover ellipsed nodes
+        for node in graph:
+            ellipsis_tags = node["ellipsis_tags"]
             ellipsed_nodes = []
-            # recover the ellipsed nodes
-            if ellipsis_method == "right_label" and len(ellipsis_tags) > 0:
+            if right_label and len(ellipsis_tags) > 0:
                 for ellipsed_node in ellipsis_tags:
                     ellipsed_node_i = "".join(re.findall(r"\d+", ellipsed_node))
                     ellipsed_node_tag = re.sub(ellipsed_node_i, "", ellipsed_node)
-                    # search the ellipsed node
+                    # find the ellipsed node
                     tag_i = 0
-                    for node in tokens:
-                        if tokens[node]["tag"] == ellipsed_node_tag:
+                    for node2 in graph:
+                        if node2["tag"] == ellipsed_node_tag:
                             if tag_i == int(ellipsed_node_i):
-                                ellipsed_nodes.append(node)
+                                ellipsed_nodes.append(node2["id"])
                             else:
-                                tag_i += 1
-            if st.height() == 2:
-                token["terminal"] = "yes"
-                token["text"] = st.leaves()[0]
-            else:
-                token["terminal"] = "no"
-                token["text"] = ""
-            tokens[index] = token
-            
-            if st.parent() != None:
-                parent_index = tree_positions[st.parent().treeposition()]
-                # add parent information
-                token["parent"] = parent_index                          # non-ellipsed children
-                for ellipsed_node in ellipsed_nodes:                    # ellipsed children                    
-                    tokens[ellipsed_node]["parent_ellipsed"].append(parent_index)
-                # add children information
-                tokens[parent_index]["children"].extend(ellipsed_nodes) # ellipsed children
-                tokens[parent_index]["children"].append(index)          # non-ellipsed children
+                                tag_i += 1         
+            for ellipsed_node in ellipsed_nodes:
+                # add ellipsed parent information
+                parent_ellipsed = graph[node["id"]+1]["parent"] if node["id"]+1 < len(graph) else graph[node["id"]]["parent"]           
+                graph[ellipsed_node]["parent_ellipsed"].append(graph[parent_ellipsed]["id"])
+                # add ellipsed children information
+                graph[parent_ellipsed]["children"].extend(ellipsed_nodes)
+            # add non-ellipsed children information
+            node["children"].append(node["id"])
 
-        graph["sentences"].append({"tokens": [tokens[token] for token in tokens]})
-    
-    return graph
+        sents["sents"].append(graph)  
+       
+    return {"docs": [sents]}
 
 
-def main(input_dir: Path, output_dir: Path, ellipsis_method: str=""):
+def main(
+    input_dir: Path, 
+    output_dir: Path, 
+    right_label: bool = typer.Option(False, help="Use right label strategy to decode ellipsis"),
+    ):
+
     for input_file in input_dir.iterdir():
-        graph = convert(input_file, ellipsis_method)
+        doc = convert(input_file, right_label)
         output_path = Path.joinpath(output_dir, input_file.name).with_suffix(".json")
         with output_path.open(mode="w+") as f:
-            json.dump(graph, f) 
+            json.dump(doc, f) 
 
 
 if __name__ == "__main__":
