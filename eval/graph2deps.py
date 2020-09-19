c9bb6cfa9621520764a5ca94acbc2c1c1e02a542
guadi1994 <guadi1994@gmail.com>
2020-09-19 21:06:06 +0200

    fixes in eval script

--- eval/graph2deps.py
+++ eval/graph2deps.py
@@ -24,7 +24,9 @@ def extract_deps(graphs):
                 token = node["text"]
                 dep_head = node["dep_head"]
                 dep_label = node["dep_label"]
-                dep.append({"token_id": token_id, "token": token, "dep_head": dep_head, "dep_label": dep_label})
+                ellipsed_dep_heads = node["ellipsed_dep_heads"]
+                ellipsed_dep_labels = node["ellipsed_dep_labels"]
+                dep.append({"token_id": token_id, "token": token, "dep_head": dep_head, "dep_label": dep_label, "ellipsed_dep_heads": ellipsed_dep_heads, "ellipsed_dep_labels": ellipsed_dep_labels})
         deps.append(dep)
 
     return deps
@@ -92,10 +94,15 @@ def add_deps(graph):
         if node["terminal"] == "no":
             dep_head = None
             dep_label = None
+            ellipsed_dep_heads = []
+            ellipsed_dep_labels = []
         elif node["terminal"] == "yes":
             dep_head, dep_label = find_dep_head(graph, node)
+            ellipsed_dep_heads, ellipsed_dep_labels = find_ellipsed_dep_heads(graph, node)
         node["dep_head"] = dep_head
         node["dep_label"] = dep_label
+        node["ellipsed_dep_heads"] = ellipsed_dep_heads
+        node["ellipsed_dep_labels"] = ellipsed_dep_labels
 
 
 def find_dep_head(graph, node):
@@ -111,6 +118,22 @@ def find_dep_head(graph, node):
             return parent["const_head"], "CLX"
         else:
             return find_dep_head(graph, parent)
+
+
+def find_ellipsed_dep_heads(graph, node):
+    """
+    """
+
+    ellipsed_parents = [graph[ellipsed_parent] for ellipsed_parent in node["ellipsed_parents"]]
+    ellipsed_dep_heads = []
+    ellipsed_dep_labels = []
+
+    for ellipsed_parent in ellipsed_parents:
+        ellipsed_dep_head, ellipsed_dep_label = find_dep_head(graph, ellipsed_parent)
+        ellipsed_dep_heads.append(ellipsed_dep_head)
+        ellipsed_dep_labels.append(ellipsed_dep_label)
+
+    return ellipsed_dep_heads, ellipsed_dep_labels
 
 
 def edge_is_correct(gold_node, predicted_node):
@@ -133,28 +156,106 @@ def label_is_correct(gold_node, predicted_node):
         return False
 
 
-def score(gold_deps, predicted_deps):
+def ellipsed_edges_are_correct(gold_node, predicted_node):
     """
     """
+
+    if set(gold_node["ellipsed_dep_heads"]) == set(predicted_node["ellipsed_dep_heads"]):
+        return True
+    else:
+        return False
+
+
+def ellipsed_labels_are_correct(gold_node, predicted_node):
+    """
+    """
+
+    gold_tuples = set()
+    for gold_head, gold_label in zip(gold_node["ellipsed_dep_heads"], gold_node["ellipsed_dep_labels"]):
+        gold_tuples.add((gold_head, gold_label))
+
+    predicted_tuples = set()
+    for predicted_head, predicted_label in zip(predicted_node["ellipsed_dep_heads"], predicted_node["ellipsed_dep_labels"]):
+        predicted_tuples.add((predicted_head, predicted_label))
+
+    if gold_tuples == predicted_tuples:
+        return True
+    else:
+        return False
+
+
+def score(gold_deps, predicted_deps, ellipsis_only=False, exclude_ellipsis=False):
+    """
+    """
+
+    if ellipsis_only:
+        print("Results for ellipsed nodes only")
+    elif exclude_ellipsis:
+        print("Results for non-ellipsed nodes only")
+    else:
+        print("Results for all nodes")
 
     correct_unlabeled = 0
     correct_labeled = 0
-    total_nodes = 0
+    total_gold_nodes = 0
+    total_predicted_nodes = 0
 
     for gold_dep, predicted_dep in zip(gold_deps, predicted_deps):
-        assert len(gold_dep) == len(predicted_dep)
-        total_nodes += len(gold_dep)
         for gold_node, predicted_node in zip(gold_dep, predicted_dep):
-            if edge_is_correct(gold_node, predicted_node):
-                correct_unlabeled += 1
-                if label_is_correct(gold_node, predicted_node):
-                    correct_labeled += 1
+            # count total of nodes
+            if ellipsis_only:
+                if len(gold_node["ellipsed_dep_heads"]) > 0:
+                    total_gold_nodes += 1
+                if len(predicted_node["ellipsed_dep_heads"]) > 0:
+                    total_predicted_nodes += 1
+            else:
+                total_gold_nodes += 1
+                total_predicted_nodes += 1
+            # count total of correct nodes
+            if ellipsis_only:
+                if len(gold_node["ellipsed_dep_heads"]) > 0:
+                    if ellipsed_edges_are_correct(gold_node, predicted_node):
+                        correct_unlabeled += 1
+                    if ellipsed_labels_are_correct(gold_node, predicted_node):
+                        correct_labeled += 1
+#                    else:
+#                        print("\n#############\n")
+#                        print("Wrong node: " + str(gold_node["token_id"]) + "\n")
+#                        for node in gold_dep:
+#                            print(node)
+#                       print("\n")
+#                        for node in predicted_dep:
+#                            print(node)
+            elif exclude_ellipsis:
+                if edge_is_correct(gold_node, predicted_node):
+                    correct_unlabeled += 1
+                    if label_is_correct(gold_node, predicted_node):
+                        correct_labeled += 1
+            else:
+                if edge_is_correct(gold_node, predicted_node) and ellipsed_edges_are_correct(gold_node, predicted_node):
+                    correct_unlabeled += 1
+                    if label_is_correct(gold_node, predicted_node) and ellipsed_labels_are_correct(gold_node, predicted_node):
+                        correct_labeled += 1 
 
-    uas = correct_unlabeled / total_nodes
-    las = correct_labeled / total_nodes
+    print(total_gold_nodes)
+    print(total_predicted_nodes) 
+    print(correct_unlabeled)
+    print(correct_labeled)              
 
-    print("UAS: " + str(round(uas, 4)))
-    print("LAS: " + str(round(las, 4)))
+    unlabeled_p = correct_unlabeled / total_predicted_nodes
+    unlabeled_r = correct_unlabeled / total_gold_nodes
+    unlabeled_f = 2 * ((unlabeled_p * unlabeled_r) / (unlabeled_p + unlabeled_r))
+    labeled_p = correct_labeled / total_predicted_nodes
+    labeled_r = correct_labeled / total_gold_nodes
+    labeled_f = 2 * ((labeled_p * labeled_r) / (labeled_p + labeled_r))
+
+    print("Unlabeled Precision: " + str(round(unlabeled_p * 100, 2)))
+    print("Unlabeled Recall: " + str(round(unlabeled_r * 100, 2)))
+    print("Unlabeled F1: " + str(round(unlabeled_f * 100, 2)))
+    print("Labeled Precision: " + str(round(labeled_p * 100, 2)))
+    print("Labeled Recall: " + str(round(labeled_r * 100, 2)))
+    print("Labeled F1: " + str(round(labeled_f * 100, 2)))
+    print("\n")
 
 
 def main(
@@ -166,14 +267,14 @@ def main(
     gold_graph = [
         {"id": 0, "children": [1, 9], "parent": None, "ellipsed_parents": [], "terminal": "no", "tag": "CLX", "text": ""},
         {"id": 1, "children": [2, 3, 4], "parent": 0, "ellipsed_parents": [], "terminal": "no", "tag": "CL", "text": ""},
-        {"id": 2, "children": [], "parent": 1, "ellipsed_parents": [], "terminal": "yes", "tag": "NG", "text": "he"},
+        {"id": 2, "children": [], "parent": 1, "ellipsed_parents": [9], "terminal": "yes", "tag": "NG", "text": "he"},
         {"id": 3, "children": [], "parent": 1, "ellipsed_parents": [], "terminal": "yes", "tag": "VG", "text": "ate"},
         {"id": 4, "children": [5, 6], "parent": 1, "ellipsed_parents": [], "terminal": "no", "tag": "NG", "text": ""},
         {"id": 5, "children": [], "parent": 4, "ellipsed_parents": [], "terminal": "yes", "tag": "NG", "text": "pizza"},
         {"id": 6, "children": [7, 8], "parent": 4, "ellipsed_parents": [], "terminal": "no", "tag": "PP", "text": ""},
         {"id": 7, "children": [], "parent": 6, "ellipsed_parents": [], "terminal": "yes", "tag": "PREP", "text": "with"},
         {"id": 8, "children": [], "parent": 6, "ellipsed_parents": [], "terminal": "yes", "tag": "NG", "text": "anchovies"},
-        {"id": 9, "children": [10, 11, 12], "parent": 0, "ellipsed_parents": [], "terminal": "no", "tag": "CL", "text": ""},
+        {"id": 9, "children": [10, 2, 11, 12], "parent": 0, "ellipsed_parents": [], "terminal": "no", "tag": "CL", "text": ""},
         {"id": 10, "children": [], "parent": 9, "ellipsed_parents": [], "terminal": "yes", "tag": "CC", "text": "and"},
         {"id": 11, "children": [], "parent": 9, "ellipsed_parents": [], "terminal": "yes", "tag": "VG", "text": "drank"},
         {"id": 12, "children": [], "parent": 9, "ellipsed_parents": [], "terminal": "yes", "tag": "NG", "text": "beer"},
@@ -182,14 +283,14 @@ def main(
     predicted_graph = [
         {"id": 0, "children": [1, 9], "parent": None, "ellipsed_parents": [], "terminal": "no", "tag": "CLX", "text": ""},
         {"id": 1, "children": [2, 3, 4], "parent": 0, "ellipsed_parents": [], "terminal": "no", "tag": "CL", "text": ""},
-        {"id": 2, "children": [], "parent": 1, "ellipsed_parents": [], "terminal": "yes", "tag": "NG", "text": "he"},
+        {"id": 2, "children": [], "parent": 1, "ellipsed_parents": [9], "terminal": "yes", "tag": "NG", "text": "he"},
         {"id": 3, "children": [], "parent": 1, "ellipsed_parents": [], "terminal": "yes", "tag": "VG", "text": "ate"},
         {"id": 4, "children": [5, 6], "parent": 1, "ellipsed_parents": [], "terminal": "no", "tag": "NG", "text": ""},
         {"id": 5, "children": [], "parent": 4, "ellipsed_parents": [], "terminal": "yes", "tag": "NG", "text": "pizza"},
         {"id": 6, "children": [7, 8], "parent": 4, "ellipsed_parents": [], "terminal": "no", "tag": "PP", "text": ""},
         {"id": 7, "children": [], "parent": 6, "ellipsed_parents": [], "terminal": "yes", "tag": "PREP", "text": "with"},
         {"id": 8, "children": [], "parent": 6, "ellipsed_parents": [], "terminal": "yes", "tag": "NG", "text": "anchovies"},
-        {"id": 9, "children": [10, 11, 12], "parent": 0, "ellipsed_parents": [], "terminal": "no", "tag": "CL", "text": ""},
+        {"id": 9, "children": [10, 2, 11, 12], "parent": 0, "ellipsed_parents": [], "terminal": "no", "tag": "CL", "text": ""},
         {"id": 10, "children": [], "parent": 9, "ellipsed_parents": [], "terminal": "yes", "tag": "CC", "text": "and"},
         {"id": 11, "children": [], "parent": 9, "ellipsed_parents": [], "terminal": "yes", "tag": "VG", "text": "drank"},
         {"id": 12, "children": [], "parent": 9, "ellipsed_parents": [], "terminal": "yes", "tag": "NG", "text": "beer"},
@@ -205,8 +306,8 @@ def main(
     predicted_deps = extract_deps(predicted_graphs)
 
     score(gold_deps, predicted_deps)
-
-    
+    score(gold_deps, predicted_deps, ellipsis_only=True)
+    score(gold_deps, predicted_deps, exclude_ellipsis=True)
 
 
 if __name__ == "__main__":
